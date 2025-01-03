7ad7a1bb8dad1895baacca3871f1d2aa6a817d66
Alekhya <alekhya1512@gmail.com>
2025-01-03 14:57:11 -0800

    Exploring routing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+
+app.get("/user", (req,res) =>{
+    res.send({firstName : "Alekhya", lastName: "Vishnu"});
+});
+app.post("/user", (req,res) =>{
+    res.send("Data Saved successsfully!")
+})
 app.use("/test", (req,res) => {
     res.send("Hello from the Server");
 });
-app.use("/hello", (req,res) => {
-    res.send("Hello Alekhya!");
-});
+
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000");
 });
