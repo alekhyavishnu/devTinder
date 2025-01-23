791ae1dd1af74d9e5fb0886d6c9e3050e806f47d
Alekhya <alekhya1512@gmail.com>
2025-01-22 17:29:05 -0800

    Adding Restful methods

--- src/app.js
+++ src/app.js
@@ -2,19 +2,36 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+app.use(express.json());
 app.post("/signup", async (req,res) =>{
-    const user = new User({
-        firstName: "Tarun",
-        lastName: "Thota",
-        emailID: "tarun@gmail.com",
-        password: "uhd38yh",
-        age: 29,
-        gender: "Male"
-    });
-await user.save();
+    const user = new User(req.body);
+try{
+    await user.save();
 res.send("User Added Successfully")
+} catch(err){
+    res.status(400).send("Error saving the user:" + err.message);
+}
 });
-
+app.get("/user", async (req,res)=>{
+ const userEmail =req.body.emailID;
+ try{
+    const users= await User.find({emailID: userEmail});
+    if(users.length === 0 ){
+        res.status(404).send("User not found");
+    } else{
+    res.send(users);
+ } }catch(err){
+    res.status(400).send("Something went wrong");
+ }
+});
+app.get("/feed", async (req,res) =>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+    res.status(400).send("Something went wrong");
+ }
+});
 
 
 connectDB().then(() =>{
